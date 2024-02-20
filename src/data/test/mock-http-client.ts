0c05ac34133ec4719ecc0a9f6d63f4525c2fecd8
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-02-20 14:37:22 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,11 +1,21 @@
 import type { HttpPostClient, HttpPostClientParams } from '@/data/protocols/http/http-post-client'
+import type { HttpResponse } from '@/data/protocols/http/http-response'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 
 export class HttpPostClientSpy implements HttpPostClient {
+  private readonly defaultResponse: HttpResponse = { statusCode: HttpStatusCode.noContent }
+
   public url?: string
   public body?: object
+  public response: HttpResponse = this.defaultResponse
 
-  async post (params: HttpPostClientParams): Promise<void> {
+  async post (params: HttpPostClientParams): Promise<HttpResponse> {
     this.url = params.url
     this.body = params.body
+    return await Promise.resolve(this.response)
+  }
+
+  reset (): void {
+    this.response = this.defaultResponse
   }
 }
