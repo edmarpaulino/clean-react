9fef3e94a1de6af3b02ec1c608253b65dc57be29
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-02-22 16:52:48 -0300

    fix: move global styles to index

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,6 +3,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Router } from '@/presentation/components'
+import '@/presentation/styles/global.scss'
 
 const domNode = document.getElementById('main')
 const root = createRoot(domNode!)
