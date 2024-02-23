dfb902f73111b4b1749464dbf76278431ab4bcc2
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-02-23 10:50:09 -0300

    test: ensure Login present valid state if password is valid

--- src/presentation/test/mock-validation.ts
+++ src/presentation/test/mock-validation.ts
@@ -2,7 +2,7 @@ import type { Validation } from '@/presentation/protocols/validation'
 import { faker } from '@faker-js/faker'
 
 export class ValidationStub implements Validation {
-  public readonly errorMessage: string = faker.word.words()
+  public errorMessage: string | null = faker.word.words()
 
   validate(fieldName: string, fieldValue: string): string | null {
     return this.errorMessage
