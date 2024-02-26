b48f39d6bf1e03ccd18f683cba2872fbdc65e0d8
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-02-26 20:03:24 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -1,10 +1,18 @@
 import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from './email-validation'
+import { faker } from '@faker-js/faker'
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', () => {
-    const sut = new EmailValidation('email')
-    const error = sut.validate('invalid_email@mail.com')
-    expect(error).toEqual(new InvalidFieldError('email'))
+    const field: string = faker.database.column()
+    const sut = new EmailValidation(field)
+    const error = sut.validate(faker.word.adjective())
+    expect(error).toEqual(new InvalidFieldError(field))
+  })
+
+  test('Should return falsy if email is valid', () => {
+    const sut = new EmailValidation(faker.database.column())
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
