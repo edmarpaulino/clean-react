40dc045ef13130482d817b785da5c311a070ad6d
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-02-28 18:44:35 -0300

    chore: disable no-non-null-assertion rule on eslint

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,25 +1,26 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
-
 import type { Authentication, AuthenticationParams } from '@/domain/usecases'
 import { HttpStatusCode, type HttpPostClient } from '@/data/protocols/http'
 import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
 import type { AccountModel } from '@/domain/models'
 
 export class RemoteAuthentication implements Authentication {
-  constructor (
+  constructor(
     private readonly url: string,
     private readonly httpPostClient: HttpPostClient<AuthenticationParams, AccountModel>
   ) {}
 
-  async auth (params: AuthenticationParams): Promise<AccountModel> {
+  async auth(params: AuthenticationParams): Promise<AccountModel> {
     const httpResponse = await this.httpPostClient.post({
       url: this.url,
       body: params
     })
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return httpResponse.body!
-      case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
-      default: throw new UnexpectedError()
+      case HttpStatusCode.ok:
+        return httpResponse.body!
+      case HttpStatusCode.unauthorized:
+        throw new InvalidCredentialsError()
+      default:
+        throw new UnexpectedError()
     }
   }
 }
