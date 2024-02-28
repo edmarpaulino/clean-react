d6f2d4874442a83eee514415764e2d8ee729f751
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-02-28 18:26:26 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct URL

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,4 +1,4 @@
-import type { AuthenticationParams } from '@/domain/usecases'
+import type { AuthenticationParams, AddAccountParams } from '@/domain/usecases'
 import { faker } from '@faker-js/faker'
 import type { AccountModel } from '@/domain/models'
 
@@ -6,6 +6,16 @@ export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
   password: faker.internet.password()
 })
+
+export const mockAddAccountParams = (): AddAccountParams => {
+  const password: string = faker.internet.password()
+  return {
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+    password,
+    passwordConfirmation: password
+  }
+}
 
 export const mockAccountModel = (): AccountModel => ({
   accessToken: faker.string.uuid()
