0686fddbde637c7da209abfb8667dff3c6aed0ca
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-02-29 10:25:50 -0300

    test: ensure CompareFieldsValidation returns falsy if compare is valid

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -12,4 +12,11 @@ describe('CompareFieldValidation', () => {
     const error = sut.validate(faker.word.sample())
     expect(error).toEqual(new InvalidFieldError(sut.field))
   })
+
+  test('Should return falsy if compare is valid', () => {
+    const valueToCompare = faker.word.sample()
+    const sut = makeSut(valueToCompare)
+    const error = sut.validate(valueToCompare)
+    expect(error).toBeFalsy()
+  })
 })
