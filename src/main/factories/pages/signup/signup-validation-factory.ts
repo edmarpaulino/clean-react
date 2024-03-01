5783d52fa71ff25cd32cafaa6c7e53723cbccb15
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-03-01 20:12:52 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/signup/signup-validation-factory.ts
@@ -0,0 +1,12 @@
+import { ValidationBuilder } from '@/validation/validators/builder/validation-builder'
+import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
+import type { Validation } from '@/presentation/protocols/validation'
+
+export const makeSignUpValidation = (): Validation => {
+  return ValidationComposite.build([
+    ...ValidationBuilder.field('name').required().min(5).build(),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().min(5).build(),
+    ...ValidationBuilder.field('passwordConfirmation').required().min(5).sameAs('password').build()
+  ])
+}
