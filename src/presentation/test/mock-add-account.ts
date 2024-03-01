90d57e330026b515e1127a44a96494921fe88c7a
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-03-01 18:14:54 -0300

    test: ensure SignUp prevent AddAccount to be called multiple times

--- src/presentation/test/mock-add-account.ts
+++ src/presentation/test/mock-add-account.ts
@@ -3,10 +3,12 @@ import { mockAccountModel } from '@/domain/test'
 import type { AddAccount, AddAccountParams } from '@/domain/usecases'
 
 export class AddAccountSpy implements AddAccount {
+  public callsCount: number = 0
   public params?: AddAccountParams
   public account: AccountModel = mockAccountModel()
 
   async add(params: AddAccountParams): Promise<AccountModel> {
+    this.callsCount++
     this.params = params
     return this.account
   }
