c39a283b31030b425f66d9013b141c9c4ba8f93b
Edmar Paulino <edmarpaulino9@gmail.com>
2024-04-01 16:55:53 -0300

    test: ensure login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,14 +1,48 @@
+import { faker } from '@faker-js/faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
   })
 
   it('Should load with correct initial state', () => {
+    cy.getByTestId('email').should('have.attr', 'readOnly')
     cy.getByTestId('email-status')
       .should('have.attr', 'title', 'Campo obrigatório')
       .should('contain.text', '🔴')
+    cy.getByTestId('email').should('have.attr', 'readOnly')
     cy.getByTestId('password-status')
       .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.word.sample())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo inválido')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password')
+      .focus()
+      .type(faker.word.sample({ length: 3 }))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo inválido')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.word.sample())
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo inválido')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password')
+      .focus()
+      .type(faker.word.sample({ length: 3 }))
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo inválido')
       .should('contain.text', '🔴')
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
