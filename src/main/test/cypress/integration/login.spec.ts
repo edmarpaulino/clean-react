13cb76bd68a656c73d570d92d030f7e69d6470de
Edmar Paulino <edmarpaulino9@gmail.com>
2024-04-02 17:46:27 -0300

    test: ensure login will not submit form if its invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -153,4 +153,16 @@ describe('Login', () => {
     cy.getByTestId('submit').click().click()
     cy.get('@request.all').should('have.length', 1)
   })
+
+  it('Should not call submit if form is invalid', () => {
+    cy.intercept('POST', /login/, {
+      delay: 500,
+      statusCode: 200,
+      body: {
+        accessToken: faker.string.uuid()
+      }
+    }).as('request')
+    cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+    cy.get('@request.all').should('have.length', 0)
+  })
 })
