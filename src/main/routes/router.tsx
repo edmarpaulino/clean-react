9ebfe8b0e8e85d2973e1301a0f93e848b05b0cc8
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-06-22 17:08:05 -0300

    feat: ensure PrivateRoute renders components if token is not empty

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeSignUp } from '@/main/factories/pages/signup/signup-factory'
 import { ApiContext } from '@/presentation/contexts'
-import { setCurrentAccountAdapter } from '@/main/adpters/current-account-adapter'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adpters/current-account-adapter'
 import { SurveyList } from '@/presentation/pages'
 
 const Router: React.FC = () => {
+  const value = useMemo(
+    () => ({
+      setCurrentAccount: setCurrentAccountAdapter,
+      getCurrentAccount: getCurrentAccountAdapter
+    }),
+    [setCurrentAccountAdapter, getCurrentAccountAdapter]
+  )
+
   const router = createBrowserRouter([
     {
       path: '/login',
@@ -23,7 +31,7 @@ const Router: React.FC = () => {
   ])
 
   return (
-    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountAdapter }}>
+    <ApiContext.Provider value={value}>
       <RouterProvider router={router} />
     </ApiContext.Provider>
   )
--- src/presentation/components/private-route/private-route.tsx
+++ src/presentation/components/private-route/private-route.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
-import { Navigate, type RouteProps } from 'react-router-dom'
+import { ApiContext } from '@/presentation/contexts'
+import React, { useContext } from 'react'
+import { Navigate, Route, type RouteProps } from 'react-router-dom'
 
 const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
-  return <Navigate to="/login" replace />
+  const { getCurrentAccount } = useContext(ApiContext)
+
+  return getCurrentAccount!()?.accessToken ? <Route {...props} /> : <Navigate to="/login" replace />
 }
 
 export default PrivateRoute
