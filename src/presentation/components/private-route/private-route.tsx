8ee3a46e28326c4ec27a632e5ea6b79d9a0d7d81
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-06-22 17:17:04 -0300

    fix: ensure PrivateRoute returns the children element if token is not empty

--- src/presentation/components/private-route/private-route.tsx
+++ src/presentation/components/private-route/private-route.tsx
@@ -1,11 +1,11 @@
 import { ApiContext } from '@/presentation/contexts'
 import React, { useContext } from 'react'
-import { Navigate, Route, type RouteProps } from 'react-router-dom'
+import { Navigate, type RouteProps } from 'react-router-dom'
 
-const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
+const PrivateRoute: React.FC<RouteProps> = ({ children }) => {
   const { getCurrentAccount } = useContext(ApiContext)
 
-  return getCurrentAccount!()?.accessToken ? <Route {...props} /> : <Navigate to="/login" replace />
+  return getCurrentAccount!()?.accessToken ? children : <Navigate to="/login" replace />
 }
 
 export default PrivateRoute
