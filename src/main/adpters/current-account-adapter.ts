6c027fd1adbde93e2bb48c13a6588755d2f7fe0f
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-06-29 11:00:57 -0300

    feat: update setCurrentAccountAdapter param

--- src/main/adpters/current-account-adapter.ts
+++ src/main/adpters/current-account-adapter.ts
@@ -1,7 +1,7 @@
 import type { AccountModel } from '@/domain/models'
 import { makeLocalStorageAdatper } from '@/main/factories/cache/local-storage-adapter-factory'
 
-export const setCurrentAccountAdapter = (account: AccountModel): void => {
+export const setCurrentAccountAdapter = (account: AccountModel | null | undefined): void => {
   const localStorageAdapter = makeLocalStorageAdatper()
   localStorageAdapter.set('account', account)
 }
