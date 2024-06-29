0784397ddd9658d0e6083bdb4b78b9917f729aff
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-06-29 11:07:41 -0300

    refactor: move sut creation to a factory method

--- src/presentation/components/header/header.spec.tsx
+++ src/presentation/components/header/header.spec.tsx
@@ -4,21 +4,34 @@ import { Header } from '@/presentation/components'
 import { ApiContext } from '@/presentation/contexts'
 import { createMemoryRouter, type RouteObject, RouterProvider } from 'react-router-dom'
 
+type SutTypes = {
+  router: React.ComponentProps<typeof RouterProvider>['router']
+  setCurrentAccountMock: typeof jest.fn
+}
+
+const makeSut = (): SutTypes => {
+  const setCurrentAccountMock = jest.fn()
+  const routes: RouteObject[] = [
+    {
+      path: '/',
+      element: <Header />
+    }
+  ]
+  const router = createMemoryRouter(routes, { initialEntries: ['/'], initialIndex: 0 })
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
+      <RouterProvider router={router} />
+    </ApiContext.Provider>
+  )
+  return {
+    router,
+    setCurrentAccountMock
+  }
+}
+
 describe('Header Component', () => {
   test('Should call setCurrentAccount with undefined', () => {
-    const setCurrentAccountMock = jest.fn()
-    const routes: RouteObject[] = [
-      {
-        path: '/',
-        element: <Header />
-      }
-    ]
-    const router = createMemoryRouter(routes, { initialEntries: ['/'], initialIndex: 0 })
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock }}>
-        <RouterProvider router={router} />
-      </ApiContext.Provider>
-    )
+    const { router, setCurrentAccountMock } = makeSut()
     fireEvent.click(screen.getByTestId('logout'))
     expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
     expect(router.state.location.pathname).toBe('/login')
