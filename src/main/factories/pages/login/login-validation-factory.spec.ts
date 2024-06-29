5ba4e60a9682cb9167c8aa2a65c413f883e9d590
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-06-29 11:58:23 -0300

    refactor: change validation factory to not use builder

--- src/main/factories/pages/login/login-validation-factory.spec.ts
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -1,14 +1,16 @@
 import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
 import { makeLoginValidation } from './login-validation-factory'
-import { ValidationBuilder } from '@/validation/validators/builder/validation-builder'
+import { EmailValidation, MinLengthValidation, RequiredFieldValidation } from '@/validation/validators'
 
 describe('LoginValidationFactory', () => {
   test('Should make ValidationComposite with correct validations', () => {
     const composite = makeLoginValidation()
     expect(composite).toEqual(
       ValidationComposite.build([
-        ...ValidationBuilder.field('email').required().email().build(),
-        ...ValidationBuilder.field('password').required().min(5).build()
+        new RequiredFieldValidation('email'),
+        new EmailValidation('email'),
+        new RequiredFieldValidation('password'),
+        new MinLengthValidation('password', 5)
       ])
     )
   })
--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -1,16 +1,21 @@
 import { ValidationComposite } from '@/validation/validators/validation-composite/validation-composite'
-import { ValidationBuilder } from '@/validation/validators/builder/validation-builder'
 import { makeSignUpValidation } from './signup-validation-factory'
+import { EmailValidation, MinLengthValidation, RequiredFieldValidation } from '@/validation/validators'
+import { CompareFieldsValidation } from '@/validation/validators/compare-fields/compare-fields-validation'
 
 describe('LoginValidationFactory', () => {
   test('Should make ValidationComposite with correct validations', () => {
     const composite = makeSignUpValidation()
     expect(composite).toEqual(
       ValidationComposite.build([
-        ...ValidationBuilder.field('name').required().min(5).build(),
-        ...ValidationBuilder.field('email').required().email().build(),
-        ...ValidationBuilder.field('password').required().min(5).build(),
-        ...ValidationBuilder.field('passwordConfirmation').required().sameAs('password').build()
+        new RequiredFieldValidation('name'),
+        new MinLengthValidation('name', 5),
+        new RequiredFieldValidation('email'),
+        new EmailValidation('email'),
+        new RequiredFieldValidation('password'),
+        new MinLengthValidation('password', 5),
+        new RequiredFieldValidation('passwordConfirmation'),
+        new CompareFieldsValidation('passwordConfirmation', 'password')
       ])
     )
   })
