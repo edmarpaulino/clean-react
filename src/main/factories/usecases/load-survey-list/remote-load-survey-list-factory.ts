125e69dd2076369ca95821214436cdfcce35d112
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-06-29 09:46:08 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/factories/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,8 +1,11 @@
-import { makeAxiosHttpClient } from '@/main/factories/http/axios-http-client-factory'
 import { makeApiUrl } from '@/main/factories/http/api-url-factory'
 import type { LoadSurveyList } from '@/domain/usecases'
 import { RemoteLoadSurveyList } from '@/data/usecases/load-survey-list/remote-load-survey-list'
+import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators'
 
 export const makeRemoteLoadSurveyList = (): LoadSurveyList => {
-  return new RemoteLoadSurveyList(makeApiUrl('/surveys'), makeAxiosHttpClient())
+  return new RemoteLoadSurveyList(
+    makeApiUrl('/surveys'),
+    makeAuthorizeHttpGetClientDecorator<RemoteLoadSurveyList.Model[]>()
+  )
 }
