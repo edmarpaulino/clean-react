f78ca20cbe6724c160a64618f279de6727a6a735
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-06-29 10:32:22 -0300

    refactor: remove unused code

--- src/main/adpters/current-account-adapter.spec.ts
+++ src/main/adpters/current-account-adapter.spec.ts
@@ -1,7 +1,6 @@
 import { mockAccountModel } from '@/domain/test'
 import { getCurrentAccountAdapter, setCurrentAccountAdapter } from './current-account-adapter'
 import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter'
-import { UnexpectedError } from '@/domain/errors'
 
 jest.mock('@/infra/cache/local-storage-adapter')
 
@@ -11,13 +10,6 @@ describe('CurrentAccountAdapter', () => {
     const setSpy = jest.spyOn(LocalStorageAdapter.prototype, 'set')
     setCurrentAccountAdapter(account)
     expect(setSpy).toHaveBeenCalledWith('account', account)
-  })
-
-  test('Should throw UnexpectedError', () => {
-    expect(() => {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      setCurrentAccountAdapter(undefined as any)
-    }).toThrow(new UnexpectedError())
   })
 
   test('Should call LocalStorageAdapter.get with correct value', () => {
--- src/main/adpters/current-account-adapter.ts
+++ src/main/adpters/current-account-adapter.ts
@@ -1,11 +1,7 @@
-import { UnexpectedError } from '@/domain/errors'
 import type { AccountModel } from '@/domain/models'
 import { makeLocalStorageAdatper } from '@/main/factories/cache/local-storage-adapter-factory'
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
-  if (!account?.accessToken) {
-    throw new UnexpectedError()
-  }
   const localStorageAdapter = makeLocalStorageAdatper()
   localStorageAdapter.set('account', account)
 }
