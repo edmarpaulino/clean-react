2edb7133ae202a2e3cc0b7cb932b36ef71d94ee6
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-06-29 16:57:48 -0300

    chore: add webpack prod and dev configs

---
+++ webpack.dev.js
@@ -0,0 +1,49 @@
+const { DefinePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const common = require('./webpack.common')
+const { merge } = require('webpack-merge')
+
+module.exports = merge(common, {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    devMiddleware: {
+      writeToDisk: true
+    },
+    static: {
+      directory: './public'
+    },
+    historyApiFallback: true
+  },
+  plugins: [
+    new DefinePlugin({ 'process.env.API_URL': JSON.stringify('http://localhost:5050/api') }),
+    new HtmlWebpackPlugin({
+      template: './template.dev.html'
+    })
+  ]
+})
