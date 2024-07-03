77ed535758c496c2c2a02e263f02c93d5012ec31
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-07-03 08:13:47 -0300

    feat: ensure RemoteLoadSurveyResult calls HttpGetClient with correct URL

---
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -0,0 +1,9 @@
+import type { HttpGetClient } from '@/data/protocols/http'
+
+export class RemoteLoadSurveyResult {
+  constructor(private readonly url: string, private readonly httpGetClient: HttpGetClient) {}
+
+  async load(): Promise<void> {
+    await this.httpGetClient.get({ url: this.url })
+  }
+}
