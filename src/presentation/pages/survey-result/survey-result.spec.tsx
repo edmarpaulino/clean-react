4651bd3d9160e8b0e02176ee604f9f1e485d27fc
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-07-04 19:07:50 -0300

    test: ensure SurveyResult presents correct initial state

---
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import { SurveyResult } from '@/presentation/pages'
+import { createMemoryRouter, type RouteObject, RouterProvider } from 'react-router-dom'
+import { ApiContext } from '@/presentation/contexts'
+
+describe('SurveyResult Component', () => {
+  test('Should present correct initial state', () => {
+    const routes: RouteObject[] = [
+      {
+        path: '/surveys',
+        element: <SurveyResult />
+      }
+    ]
+    const router = createMemoryRouter(routes, { initialEntries: ['/surveys'], initialIndex: 0 })
+    render(
+      <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: jest.fn() }}>
+        <RouterProvider router={router} />
+      </ApiContext.Provider>
+    )
+    const surveyResult = screen.getByTestId('survey-result')
+    expect(surveyResult.childElementCount).toBe(0)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+  })
+})
