ac1dcbe93ee9af8b27ff4233d222da74ea7db280
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-07-04 19:09:11 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/survey-result/survey-result.spec.tsx
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -4,20 +4,24 @@ import { SurveyResult } from '@/presentation/pages'
 import { createMemoryRouter, type RouteObject, RouterProvider } from 'react-router-dom'
 import { ApiContext } from '@/presentation/contexts'
 
+const makeSut = (): void => {
+  const routes: RouteObject[] = [
+    {
+      path: '/surveys',
+      element: <SurveyResult />
+    }
+  ]
+  const router = createMemoryRouter(routes, { initialEntries: ['/surveys'], initialIndex: 0 })
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: jest.fn() }}>
+      <RouterProvider router={router} />
+    </ApiContext.Provider>
+  )
+}
+
 describe('SurveyResult Component', () => {
   test('Should present correct initial state', () => {
-    const routes: RouteObject[] = [
-      {
-        path: '/surveys',
-        element: <SurveyResult />
-      }
-    ]
-    const router = createMemoryRouter(routes, { initialEntries: ['/surveys'], initialIndex: 0 })
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: jest.fn() }}>
-        <RouterProvider router={router} />
-      </ApiContext.Provider>
-    )
+    makeSut()
     const surveyResult = screen.getByTestId('survey-result')
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.queryByTestId('error')).not.toBeInTheDocument()
