bd910fc145a2d6e03525024c365f16d495c70817
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-07-05 08:11:28 -0300

    test: ensure SurveyResult reload on reload button click

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -7,6 +7,12 @@ const mockUnexpectedError = (): void => {
   Http.mockServerError(PATH, 'GET')
 }
 
+const mockSuccess = (): void => {
+  cy.fixture('survey-result').then((surveyResult) => {
+    Http.mockOk(PATH, 'GET', surveyResult)
+  })
+}
+
 describe('SurveyResult', () => {
   beforeEach(() => {
     cy.fixture('account').then((account: object) => {
@@ -18,5 +24,14 @@ describe('SurveyResult', () => {
     mockUnexpectedError()
     cy.visit('/surveys/any_id')
     cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+  })
+
+  it('Should reaload on button click', () => {
+    mockUnexpectedError()
+    cy.visit('/surveys/any_id')
+    cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.')
+    mockSuccess()
+    cy.getByTestId('reload').click()
+    cy.getByTestId('question').should('exist')
   })
 })
