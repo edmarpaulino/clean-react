c2f6839e16664de2e757e98395d9709575c60a97
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-07-05 15:48:56 -0300

    feat: ensure SurveyResult presents new result on answer click

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -24,7 +24,12 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult, saveSurveyResult }) =
 
   const onAnswer = (answer: string): void => {
     setState((prev) => ({ ...prev, isLoading: true }))
-    saveSurveyResult.save({ answer }).then().catch(handleError)
+    saveSurveyResult
+      .save({ answer })
+      .then((surveyResult) => {
+        setState((prev: any) => ({ ...prev, isLoading: false, surveyResult }))
+      })
+      .catch(handleError)
   }
 
   const reload = (): void => {
