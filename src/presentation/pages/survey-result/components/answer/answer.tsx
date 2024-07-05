ff3a0825cf365b248709702f460bcc0556d5ee7e
Edmar Paulino <edmar.paulino@grupoboticario.com.br>
2024-07-04 21:07:57 -0300

    refactor: move SurveyResultAnswer to its own Component

---
+++ src/presentation/pages/survey-result/components/answer/answer.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Styles from './answer-styles.scss'
+
+type Props = {
+  answer: {
+    image?: string
+    answer: string
+    count: number
+    percent: number
+    isCurrentAccountAnswer: boolean
+  }
+}
+
+const Answer: React.FC<Props> = ({ answer }) => {
+  const activeClassName = answer.isCurrentAccountAnswer ? Styles.active : ''
+
+  return (
+    <li data-testid="answer-wrap" key={answer.answer} className={[Styles.answerWrap, activeClassName].join(' ')}>
+      {answer.image && <img data-testid="image" src={answer.image} alt={answer.answer} />}
+      <span data-testid="answer" className={Styles.answer}>
+        {answer.answer}
+      </span>
+      <span data-testid="percent" className={Styles.percent}>
+        {answer.percent}%
+      </span>
+    </li>
+  )
+}
+
+export default Answer
--- src/presentation/pages/survey-result/components/result/result.tsx
+++ src/presentation/pages/survey-result/components/result/result.tsx
@@ -4,6 +4,7 @@ import Styles from './result-styles.scss'
 import { Calendar } from '@/presentation/components'
 import { useNavigate } from 'react-router-dom'
 import type { LoadSurveyResult } from '@/domain/usecases'
+import { SurveyResultAnswer } from '@/presentation/pages/survey-result/components'
 
 type Props = {
   surveyResult: LoadSurveyResult.Model
@@ -23,21 +24,12 @@ const Result: React.FC<Props> = ({ surveyResult }) => {
         <h2 data-testid="question">{surveyResult.question}</h2>
       </hgroup>
       <FlipMove data-testid="answers" className={Styles.answerList}>
-        {surveyResult.answers.map((answer) => (
-          <li
-            data-testid="answer-wrap"
-            key={answer.answer}
-            className={answer.isCurrentAccountAnswer ? Styles.active : ''}
-          >
-            {answer.image && <img data-testid="image" src={answer.image} alt={answer.answer} />}
-            <span data-testid="answer" className={Styles.answer}>
-              {answer.answer}
-            </span>
-            <span data-testid="percent" className={Styles.percent}>
-              {answer.percent}%
-            </span>
-          </li>
-        ))}
+        {/* To fix FlipMovePropConverter error */}
+        <>
+          {surveyResult.answers.map((answer) => (
+            <SurveyResultAnswer key={answer.answer} answer={answer} />
+          ))}
+        </>
       </FlipMove>
       <button className={Styles.button} data-testid="back-button" onClick={goBack}>
         Voltar
